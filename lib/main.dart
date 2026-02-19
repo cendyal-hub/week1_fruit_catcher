0bc1990a3f86832c12c5f73a1ce929c13b6f847b
cendyal-hub <sendyaja17@gmail.com>
2026-02-19 13:46:39 +0700

    remove expanded and column

--- lib/main.dart
+++ lib/main.dart
@@ -19,76 +19,71 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
-  final ValueNotifier<int> counter = ValueNotifier(1);
 
   @override
   void initState() {
     super.initState();
   }
+  final ValueNotifier<int> counter = ValueNotifier(0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 50,
-                  left: 20,
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-
-                    child: ValueListenableBuilder<int>(
-                      valueListenable: counter,
-                      builder: (context, score, child) {
-                        return Text(
-                          'Score: $score',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        );
-                      },
-                    ),
+      body: Stack(
+            children: [
+              Positioned(
+                top: 50,
+                left: 20,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+          
+                  child: ValueListenableBuilder<int>(
+                    valueListenable: counter,
+                    builder: (context, score, child) {
+                      return Text(
+                        'Score: $score',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                    },
                   ),
                 ),
-                Positioned(
-                  top: 50,
-                  right: 20,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.music_note, color: Colors.black),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.volume_up, color: Colors.black),
-                      ),
-                    ],
-                  ),
+              ),
+              Positioned(
+                top: 50,
+                right: 20,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.music_note, color: Colors.black),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.volume_up, color: Colors.black),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: ElevatedButton(
-              onPressed: () {
-                counter.value++;
-              },
-              child: const Text("Tambah Score"),
-            ),
-          ),
-        ],
-      ),
+          // Padding(
+          //   padding: const EdgeInsets.all(16),
+          //   child: ElevatedButton(
+          //     onPressed: () {
+          //       counter.value++;
+          //     },
+          //     child: const Text("Tambah Score"),
+          //   ),
+          // ),
+        
     );
   }
 }
