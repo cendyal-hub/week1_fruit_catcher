9c8d4a9f38a1bb87129acf8ffe504c2fe6757027
cendyal-hub <sendyaja17@gmail.com>
2026-02-19 13:33:35 +0700

    change expanded to column

--- lib/main.dart
+++ lib/main.dart
@@ -29,52 +29,56 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        child: Stack(
-          children: [
-            Positioned(
-              top: 50,
-              left: 20,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.black54,
-                  borderRadius: BorderRadius.circular(10),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+            
+                    child: ValueListenableBuilder<int>(
+                      valueListenable: counter,
+                      builder: (context, score, child) {
+                        return Text(
+                          'Score: $score',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-        
-                child: ValueListenableBuilder<int>(
-                  valueListenable: counter,
-                  builder: (context, score, child) {
-                    return Text(
-                      'Score: $score',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                Positioned(
+                  top: 50,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.music_note, color: Colors.black),
                       ),
-                    );
-                  },
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.volume_up, color: Colors.black),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-            Positioned(
-              top: 50,
-              right: 20,
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.music_note, color: Colors.black),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.volume_up, color: Colors.black),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
