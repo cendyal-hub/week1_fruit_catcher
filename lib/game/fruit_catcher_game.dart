1e86b4ae454a9b1c8c280cde3e00822fb182b4a2
cendyal-hub <sendyaja17@gmail.com>
2026-02-19 14:16:27 +0700

    update file game

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -1,14 +1,16 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_catcher/game/managers/audio_manager.dart';
 
 class FruitCatcherGame extends FlameGame {
-  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
-
   @override
   Color backgroundColor() => const Color(0xFF87CEEB);
+
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    AudioManager().playBackgroundMusic();
   }
 }
